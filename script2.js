cc3b6b9d25c1e50740c6a4571b5ae9b71ab47ab0
JoaoJaskulski <160859383+JoaoJaskulski@users.noreply.github.com>
2025-04-04 07:46:14 -0300

    .

--- script2.js
+++ script2.js
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 })
+aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 diminuiFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem
